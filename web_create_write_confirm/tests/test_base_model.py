4c910ced1a4cac8f374d85f6b92c04fd68a0a9e6
Andrey <andrejsolodovnikov699@gmail.com>
2023-07-14 11:23:39 +0300

    [14.0][IMP] web_create_write_confirm
    Fix tests

--- web_create_write_confirm/tests/test_base_model.py
+++ web_create_write_confirm/tests/test_base_model.py
@@ -7,8 +7,8 @@ class TestBaseModel(SavepointCase):
         super(TestBaseModel, cls).setUpClass()
         cls.base_model = cls.env["base"]
 
-    def test_get_message_information(self):
-        self.assertFalse(self.base_model.get_message_information())
+    def test_get_message_informations(self):
+        self.assertFalse(self.base_model.get_message_informations())
 
     def test_execute_processing(self):
-        self.assertTrue(self.base_model.execute_processing())
+        self.assertFalse(self.base_model.execute_processing())
