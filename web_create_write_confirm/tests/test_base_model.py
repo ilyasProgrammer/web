1836b20ce28b72d1231d812a735eb0b1669c3293
Andrey <andrejsolodovnikov699@gmail.com>
2023-07-17 22:43:39 +0300

    [14.0][IMP] web_create_write_confirm
    Add msg to tests and description, rewrite 1 test

--- web_create_write_confirm/tests/test_base_model.py
+++ web_create_write_confirm/tests/test_base_model.py
@@ -8,7 +8,20 @@ class TestBaseModel(SavepointCase):
         cls.base_model = cls.env["base"]
 
     def test_get_message_informations(self):
-        self.assertFalse(self.base_model.get_message_informations())
+        """Test correct flow of get_message_informations method"""
+        ret_value_of_method = self.base_model.get_message_informations()
+        check_ret_value = False
+        if (ret_value_of_method is False) or isinstance(
+            ret_value_of_method, type(self.env["popup.message"])
+        ):
+            check_ret_value = True
+        self.assertTrue(
+            check_ret_value,
+            msg="Return value must be False or dictionary of popup.message objects",
+        )
 
     def test_execute_processing(self):
-        self.assertFalse(self.base_model.execute_processing())
+        """Test correct flow of execute_processing method"""
+        self.assertFalse(
+            self.base_model.execute_processing(), msg="Return value must be False"
+        )
