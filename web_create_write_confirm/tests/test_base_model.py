3cbd42ba9d8b852b9bef9e3bda79b71ada7ae053
Andrey <andrejsolodovnikov699@gmail.com>
2023-07-19 11:50:59 +0300

    [14.0][IMP] web_create_write_confirm: add new test
    Add new tests for checkint flow of _compute_field_name method

--- web_create_write_confirm/tests/test_base_model.py
+++ web_create_write_confirm/tests/test_base_model.py
@@ -7,6 +7,33 @@ class TestBaseModel(SavepointCase):
         super(TestBaseModel, cls).setUpClass()
         cls.base_model = cls.env["base"]
 
+    def test_compute_field_name(self):
+        """Test correct flow of changing field_name"""
+        model_field_name = self.env["ir.model.fields"]._get(
+            "popup.message", "field_name"
+        )
+        vals = {
+            "model_id": model_field_name.model_id.id,
+            "field_ids": model_field_name,
+            "message": "test",
+        }
+        test_popup_message = self.env["popup.message"].sudo().create(vals)
+        self.assertEqual(
+            test_popup_message.field_name,
+            model_field_name.name,
+            msg="Must be equal to name of 'ir.model.fields' field",
+        )
+
+        model_field_title = self.env["ir.model.fields"]._get("popup.message", "title")
+        vals.update({"field_ids": (model_field_name.id, model_field_title.id)})
+        test_popup_message_cycle = self.env["popup.message"].sudo().create(vals)
+        field_name = model_field_name.name + "," + model_field_title.name
+        self.assertEqual(
+            test_popup_message_cycle.field_name,
+            field_name,
+            msg="Must be equal to 2 join name of 'ir.model.fields' fields",
+        )
+
     def test_get_message_informations(self):
         """Test correct flow of get_message_informations method"""
         ret_value_of_method = self.base_model.get_message_informations()
